384b41e5ac5bff3ceb5dad2a30bc260b680edee1
Lynix <81735347+cyberlynix@users.noreply.github.com>
2023-12-16 10:36:37 -0400

    patch: sylva release lobby picture

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
 	import "../app.css";
 </script>
   
-<div class="text-white" style="background:url(/bg.png);background-position:center;background-size:cover;">
-	<div class="min-h-screen bg-gray-950 bg-opacity-90">
+<div class="text-white" style="background:url(/sylva-in-winter.png);background-position:center;background-size:cover;">
+	<div class="min-h-screen bg-gray-950 bg-opacity-70">
         <Navbar/>
 		<slot />
 	</div>
